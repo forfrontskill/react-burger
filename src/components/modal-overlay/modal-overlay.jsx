a404202eaf663ac832d9a8dc8d0d094e2746068a
alexey <kelfoz@gmail.com>
2022-09-17 18:17:15 +0300

    Рефакторинг модального окна

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,35 +1,12 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
 
 import style from './modal-overlay.module.css';
 
-
-const modalRoot = document.getElementById('react-modals');
-
-const ModalOverlay = ({ onClick, children }) => {
-
-    const overlayClick = (evt) => {
-        if(evt.target.className === style.ModalOverlay){
-            onClick();
-        }
-    }
-
-    const modal = (
-        <div className={style.ModalOverlay} onMouseDown={overlayClick}>
-            {children}
-        </div>
-    )
-
-    return ReactDOM.createPortal(modal, modalRoot)
-}
+const ModalOverlay = ({ onClick }) => <div className={style.ModalOverlay} onClick={onClick}></div>
 
 ModalOverlay.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-    ])
+    onClick: PropTypes.func.isRequired
 }
 
 export default ModalOverlay;
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import PropTypes from 'prop-types';
 import ModalOverlay from "../modal-overlay/modal-overlay";
+import ReactDOM from 'react-dom';
 
 import style from './modal.module.css';
 
+const modalRoot = document.getElementById('react-modals');
 
 const Modal = ({ title, onClose, children }) => {
 
@@ -21,15 +23,18 @@ const Modal = ({ title, onClose, children }) => {
 
     }, [onClose])
 
-    return (
-        <ModalOverlay onClick={onClose}>
+    const modal = (
+        <>
             <div className={style.Container}>
                 {title && <h2 className={'text text_type_main-large ' + style.Title}>{title}</h2>}
                 <button className={style.ButtonClose} onClick={onClose}></button>
                 {children}
             </div>
-        </ModalOverlay>
+            <ModalOverlay onClick={onClose} />
+        </>
     )
+
+    return ReactDOM.createPortal(modal, modalRoot)
 }
 
 Modal.propTypes = {
