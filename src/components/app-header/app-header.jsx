04292bdd07d71a0e8350e7eb3dd602376780e409
Алексей <avmakarov.mail@yandex.ru>
2022-11-26 17:28:07 +0300

    Первая рабочая версия

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -9,42 +9,51 @@ import style from './app-header.module.css';
 const PATH_PROFILE = '/profile';
 const PATH_BASE = '/';
 const PATH_INGREDIENTS = '/ingredients/';
+const PATH_FEED = '/feed';
 
 const AppHeader = () => {
 
     const history = useHistory();
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
 
     const isProfile = pathname.includes(PATH_PROFILE) ? 'active' : 'inactive';
     const isConstuctor = pathname === PATH_BASE || pathname.includes(PATH_INGREDIENTS) ? 'active' : 'inactive';
 
     const handleProfile = useCallback(
-        () => {
+        (e) => {
+            e.preventDefault();
             history.push({ pathname: PATH_PROFILE });
         },
         [history]
-      ); 
-    
-      const handleForwardMainPage = useCallback(
-        () => {
+    );
+
+    const handleForwardMainPage = useCallback(
+        (e) => {
+            e.preventDefault();
             history.push({ pathname: PATH_BASE });
         },
         [history]
-      ); 
+    );
+
+    const handleFeedPage = useCallback(
+        (e) => {
+            e.preventDefault();
+            history.push({ pathname: PATH_FEED });
+        },
+        [history]
+    );
 
     return (
         <header className={style.AppHeader}>
             <nav className={style.Menu}>
                 <div>
                     <MenuButton icon={<BurgerIcon />} text={'Конструктор'} type={isConstuctor} onClick={handleForwardMainPage} />
-                    <MenuButton icon={<ListIcon />} text={'Лента заказов'} type='inactive' onClick={() => { }} />
+                    <MenuButton icon={<ListIcon />} text={'Лента заказов'} type='inactive' onClick={handleFeedPage} />
                 </div>
                 <div className={style.Logo} onClick={handleForwardMainPage}>
-                <Logo />
+                    <Logo />
                 </div>
-                <MenuButton icon={<MenuIcon />} text={'Личный кабинет'} type={isProfile}  onClick={(e) => {
-                    e.preventDefault();
-                    handleProfile()}} />
+                <MenuButton icon={<MenuIcon />} text={'Личный кабинет'} type={isProfile} onClick={handleProfile} />
             </nav>
         </header>
     )
